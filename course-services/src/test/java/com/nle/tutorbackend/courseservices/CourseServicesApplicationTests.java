d2a6e109810ecc3aebc83ccb6c4d4fd822949020
Nhung Le <misssg85@gmail.com>
2022-12-13 12:11:32 -0600

    Rename user and course projects

---
+++ course-services/src/test/java/com/nle/tutorbackend/courseservices/CourseServicesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nle.tutorbackend.courseservices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CourseServicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
